340e201af887ce38de068f99e103ec231898d3ff
Yan David <ygomezcoba@gmail.com>
2022-01-10 16:45:55 -0500

    Upload files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
@@ -53,6 +53,10 @@ const routes: Routes = [
   {
     path: 'search-location',
     loadChildren: () => import('./search-location/search-location.module').then( m => m.SearchLocationPageModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./welcome/welcome.module').then( m => m.WelcomePageModule),
   }
 ];
 @NgModule({
