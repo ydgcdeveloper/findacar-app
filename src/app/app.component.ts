f50a88086745097683a66c3dd95b046d3a8bb4a7
Yan David <ygomezcoba@gmail.com>
2022-02-02 15:55:04 -0500

    Upload files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
+import { Platform, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private platform: Platform, private _location: Location, private toastController: ToastController) {
+    this.initializeApp();
+    console.log("App init");
+   }
+
+  initializeApp() {
+    this.platform.backButton.subscribeWithPriority(15, (processNextHandler) => {
+      console.log('Back press handler!');
+      if (this._location.isCurrentPathEqualTo('/tabs/home')) {
+
+        // Show Exit Alert!
+        console.log('Show Exit Alert!');
+        this.presentToast('Show Exit Alert!')
+        processNextHandler();
+      } else {
+
+        // Navigate to back page
+        console.log('Navigate to back page');
+        this.presentToast('Navigate to back page')
+        this._location.back();
+
+      }
+    })
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 3000,
+      icon: "locate-outline",
+      color: 'dark'
+    });
+    toast.present();
+  }
 }
