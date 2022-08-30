3e1ef114c692fa357afeb688f98f3c957a3760d9
Yan David <ydgcdeveloper@gmail.com>
2022-08-30 12:14:54 -0400

    refactor to set correct text and handle wrong date

--- src/app/request/request.page.ts
+++ src/app/request/request.page.ts
@@ -1,3 +1,4 @@
+import { ToastColors, ToastService, ToastPositions } from './../api/services/toast/toast.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PickerColumn, PickerColumnOption, PickerController, PickerOptions, ToastController } from '@ionic/angular';
@@ -17,32 +18,27 @@ export class RequestPage implements OnInit {
   private minute;
   private meridiam;
   private date;
+  public timeText: string;
 
   gadgets: any[] = [
     this.days,
     this.hours,
     this.minutes,
     [
-      "AM",
-      "PM",
+      'AM',
+      'PM',
     ]
   ];
 
-  constructor(private router: Router, private pickerController: PickerController, private toastCtrl: ToastController) { }
+  constructor(private router: Router, private pickerController: PickerController, private toast: ToastService) { }
 
   ngOnInit() {
     this.setPickerData();
-    setTimeout(() => {
-      console.log("Empecé")
-      this.minutes = [];
-      for (let i = 0; i < 30; i += 5) {
-        this.minutes.push(i < 10 ? '0' + i : i + '');
-      }
-    }, 10000)
+    this.timeText = 'Ahora';
   }
 
   goToHome() {
-    this.router.navigate(['tabs/home'])
+    this.router.navigate(['tabs/home']);
   }
 
   setPickerData() {
@@ -54,31 +50,31 @@ export class RequestPage implements OnInit {
       this.minutes.push(i < 10 ? '0' + i : i + '');
     }
 
-    let options: Intl.DateTimeFormatOptions = {
+    const options: Intl.DateTimeFormatOptions = {
       weekday: 'short', month: 'short', day: 'numeric',
     };
 
     this.days.push({
       realdate: new Date().toDateString(),
       dateview: 'Hoy'
-    })
+    });
     for (let index = 1; index < 30; index++) {
-      let day = add(new Date(), {
+      const day = add(new Date(), {
         days: index
-      })
+      });
 
-      let data = {
+      const data = {
         realdate: day.toDateString(),
         dateview: new Intl.DateTimeFormat('es-Es', options).format(day)
-      }
-      this.days.push(data)
+      };
+      this.days.push(data);
     }
   }
 
   async showPicker() {
     this.setTime();
 
-    let options: PickerOptions = {
+    const options: PickerOptions = {
       cssClass: 'date-picker',
       buttons: [
         {
@@ -87,22 +83,30 @@ export class RequestPage implements OnInit {
           cssClass: 'rounded',
           handler: () => {
             this.date = new Date(Date.now());
+            this.timeText = 'Ahora';
           },
-          
         },
         {
           text: 'Confirmar',
-          handler:  (value) => {
-            let date = new Date(this.gadgets[0][parseInt(value.col0.value)].realdate)
-            let hourVal = parseInt(this.gadgets[1][parseInt(value.col1.value)]);
-            date.setHours(this.gadgets[3][parseInt(value.col3.value)] == 'AM' ? hourVal == 12 ? 0 : hourVal : hourVal == 12 ? 12 : hourVal + 12)
-            date.setMinutes(this.gadgets[2][parseInt(value.col2.value)])
-            var dateNow = new Date(Date.now());
+          handler: (value) => {
+            const date = new Date(this.gadgets[0][parseInt(value.col0.value)].realdate);
+            const hourVal = parseInt(this.gadgets[1][parseInt(value.col1.value)]);
+            date.setHours(this.gadgets[3][parseInt(value.col3.value)] == 'AM' ? hourVal == 12 ? 0 : hourVal : hourVal == 12 ? 12 : hourVal + 12);
+            date.setMinutes(this.gadgets[2][parseInt(value.col2.value)]);
+            const dateNow = new Date(Date.now());
             if (+dateNow > +date) {
-              console.log('Wrong', date)
-              this.presentToast()
+              console.log('Wrong', date);
+              this.date = dateNow;
+              this.timeText = 'Ahora';
+              this.toast.showToast({ header: 'INFO', message: 'Fecha anterior a la actual', color: ToastColors.WARNING, position: ToastPositions.TOP, icon: 'information-circle' });
             } else {
-              console.log('Good!', date)
+              const optionsDate: Intl.DateTimeFormatOptions = {
+                weekday: 'long', month: 'short', day: 'numeric', hour12: true, hour: '2-digit', minute: '2-digit'
+              };
+              console.log('Good!', date);
+              console.log('Good!', new Intl.DateTimeFormat('es-Es', optionsDate).format(date));
+              this.timeText = new Intl.DateTimeFormat('es-Es', optionsDate).format(date);
+              this.timeText = this.timeText.charAt(0).toUpperCase() + this.timeText.slice(1);
             }
           },
         },
@@ -110,32 +114,22 @@ export class RequestPage implements OnInit {
 
       columns: this.getColumns(this.gadgets),
     };
-    let picker = await this.pickerController.create(options);
+    const picker = await this.pickerController.create(options);
     picker.onDidDismiss().then(() => {
       console.log('closed did dismiss');
-    })
+    });
     picker.onWillDismiss().then(() => {
       console.log('closed will dismiss');
-    })
-    picker.present()
+    });
+    picker.present();
   }
 
-  async presentToast() {
-    let toast = await  this.toastCtrl.create({
-      message: 'User was added successfully',
-      duration: 3000,
-      position: 'top',
-      animated: true,
-    });
-  
-    toast.present();
-  }
 
   setTime() {
     let option: Intl.DateTimeFormatOptions = {
       hour12: false, hour: 'numeric',
     };
-    let parse = parseInt(new Date(Date.now()).toLocaleString('es-Es', option));
+    const parse = parseInt(new Date(Date.now()).toLocaleString('es-Es', option));
     this.hour = parse == 12 ? 12 : parse % 12;
 
     option = {
@@ -143,36 +137,36 @@ export class RequestPage implements OnInit {
     };
     this.minute = new Date(Date.now()).toLocaleString('es-Es', option);
 
-    this.meridiam = new Date().getHours() >= 12 ? "PM" : "AM";
+    this.meridiam = new Date().getHours() >= 12 ? 'PM' : 'AM';
   }
 
   getColumns(elements: any[]) {
-    let columns: PickerColumn[] = [];
+    const columns: PickerColumn[] = [];
     for (let i = 0; i < elements.length; i++) {
       columns.push({
         name: `col${i}`,
         options: this.getColumnOptions(i, elements),
         selectedIndex: i == 1 || i == 2 || i == 3 ? this.checkSelected(i) : 0,
-      })
+      });
     }
     return columns;
   }
 
   getColumnOptions(columIndex: number, elements: any[]) {
 
-    let options: PickerColumnOption[] = [];
+    const options: PickerColumnOption[] = [];
     for (let i = 0; i < elements[columIndex].length; i++) {
       options.push({
         text: columIndex == 0 ? elements[columIndex][i].dateview : elements[columIndex][i],
         value: i,
-      })
+      });
     }
     return options;
   }
 
   checkSelected(columIndex: number) {
     if (columIndex == 1) {
-      return parseInt(this.hour) - 1
+      return parseInt(this.hour) - 1;
     } if (columIndex == 2) {
       if (this.minute > 54) {
         this.hour++;
