1856928fe7ceaef412947f47f4b09fb7ec26511a
Yan David <ydgcdeveloper@gmail.com>
2022-09-11 12:51:30 -0400

    refactor filter service

--- src/app/api/services/filter/filter.service.ts
+++ src/app/api/services/filter/filter.service.ts
@@ -1,51 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Category } from '../../interfaces/category/category.interface';
+import { Filter, SortByTypes } from '../../interfaces/filter/filter';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilterService {
-  public categories: Category[] = [
-    {
-      id: 1,
-      name: 'Carga pesada',
-      color: 'red',
-      description: '',
-      photo: `../../../assets/icon/heavy-96.png`,
-    },
-    {
-      id: 2,
-      name: 'Equipos pesados',
-      color: 'blue',
-      description: 'Carga y transporte de electrodomesticos hasta 1T',
-      photo: `../../../assets/images/car-motor.png`,
-    },
-    {
-      id: 3,
-      name: 'Mudanza',
-      color: 'green',
-      description: 'Para mudanza en todo el país',
-      photo: `../../../assets/icon/heavy-green-96.png`,
-    },
-    {
-      id: 4,
-      name: 'Mototaxi',
-      color: 'brown',
-      description: 'Recogida y transporte de personal a lugares publicos',
-      photo: `../../../assets/icon/motorcycle-green-96.png`,
-    },
-    {
-      id: 5,
-      name: 'Taxi',
-      color: 'yellow',
-      description: 'Recogida y transporte de personal a lugares especificos',
-      photo: `../../../assets/icon/taxi_96.png`,
-    }
-  ];
 
+  private filter: Filter = {
+    sortBy : SortByTypes.RATING,
+    onlyAvailable: true,
+    priceRange: {
+      lower: 1000,
+      upper: 6000
+    },
+    categories: []
+  }
+  
   constructor() { }
 
-  getCategories(){
-    return this.categories;
+  getFilter(){
+    return this.filter;
   }
 }
