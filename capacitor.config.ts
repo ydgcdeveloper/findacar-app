fb8743375aadd46c162f2b84d677ce72ec0ab610
Yan David <ydgcdeveloper@gmail.com>
2022-09-29 12:22:22 -0400

    set and update capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,21 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'io.ionic.starter',
   appName: 'FindACar',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  cordova: {
+    preferences: {
+      ScrollEnabled: 'false',
+      BackupWebStorage: 'none',
+      SplashMaintainAspectRatio: 'true',
+      FadeSplashScreenDuration: '300',
+      SplashShowOnlyFirstTime: 'false',
+      SplashScreen: 'screen',
+      SplashScreenDelay: '3000'
+    }
+  }
 };
 
 export default config;
